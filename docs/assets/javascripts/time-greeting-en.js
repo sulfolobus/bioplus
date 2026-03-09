14456e09e9505fa9f2a0c6d36b3793b82fd29e26
BioPlus <bioplus@example.com>
2026-03-09 18:16:58 +0800

    Simplify greeting messages

--- docs/assets/javascripts/time-greeting-en.js
+++ docs/assets/javascripts/time-greeting-en.js
@@ -4,24 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const hour = new Date().getHours();
   let greeting = "";
-  let icon = "";
 
   if (hour >= 5 && hour < 12) {
     greeting = "Good morning";
-    icon = "🌅";
   } else if (hour >= 12 && hour < 14) {
     greeting = "Good noon";
-    icon = "☀️";
   } else if (hour >= 14 && hour < 18) {
     greeting = "Good afternoon";
-    icon = "🌤️";
   } else if (hour >= 18 && hour < 22) {
     greeting = "Good evening";
-    icon = "🌙";
   } else {
     greeting = "Late night";
-    icon = "🌃";
   }
 
-  greetingElement.innerHTML = icon + " " + greeting + ", welcome to BioPlus!";
+  greetingElement.textContent = greeting;
 });
--- docs/assets/javascripts/time-greeting.js
+++ docs/assets/javascripts/time-greeting.js
@@ -4,24 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const hour = new Date().getHours();
   let greeting = "";
-  let icon = "";
 
   if (hour >= 5 && hour < 12) {
     greeting = "早上好";
-    icon = "🌅";
   } else if (hour >= 12 && hour < 14) {
     greeting = "中午好";
-    icon = "☀️";
   } else if (hour >= 14 && hour < 18) {
     greeting = "下午好";
-    icon = "🌤️";
   } else if (hour >= 18 && hour < 22) {
     greeting = "晚上好";
-    icon = "🌙";
   } else {
     greeting = "夜深了";
-    icon = "🌃";
   }
 
-  greetingElement.innerHTML = icon + " " + greeting + "，欢迎来到 BioPlus！";
+  greetingElement.textContent = greeting;
 });
